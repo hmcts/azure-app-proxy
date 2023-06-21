5fb1af82f53f84b81c8a0004465f487f06e8c5cb
Rees Pozzi <reespozzi@icloud.com>
2023-06-21 18:27:45 +0100

    Format new file

--- components/app-proxy/vm-bootstrap.tf
+++ components/app-proxy/vm-bootstrap.tf
@@ -4,22 +4,22 @@ locals {
 
 # Splunk
 data "azurerm_key_vault" "soc_vault" {
-  provider = azurerm.soc
+  provider            = azurerm.soc
   name                = "soc-prod"
   resource_group_name = "soc-core-infra-prod-rg"
 }
 data "azurerm_key_vault_secret" "splunk_username" {
-  provider = azurerm.soc
+  provider     = azurerm.soc
   name         = "splunk-gui-admin-username"
   key_vault_id = data.azurerm_key_vault.soc_vault.id
 }
 data "azurerm_key_vault_secret" "splunk_password" {
-  provider = azurerm.soc
+  provider     = azurerm.soc
   name         = "splunk-gui-admin-password"
   key_vault_id = data.azurerm_key_vault.soc_vault.id
 }
 data "azurerm_key_vault_secret" "splunk_pass4symmkey" {
-  provider = azurerm.soc
+  provider     = azurerm.soc
   name         = "Splunk-pass4SymmKey"
   key_vault_id = data.azurerm_key_vault.soc_vault.id
 }
