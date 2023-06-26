e526b4d6551f34d11a62bad1a5bb40fad24275b3
Rees Pozzi <reespozzi@icloud.com>
2023-06-26 15:12:13 +0100

    Tidy up pipeline script

--- components/app-proxy/get-access-token.sh
+++ components/app-proxy/get-access-token.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
-az account get-access-token --resource-type 'aad-graph' --scope 'https://proxy.cloudwebappproxy.net/registerapp/user_impersonation'
+az login --username $(azure-app-proxy-user-email) --password $(azure-app-proxy-user-password) --allow-no-subscriptions
+TOKEN=$(az account get-access-token --resource-type 'aad-graph' --scope 'https://proxy.cloudwebappproxy.net/registerapp/user_impersonation' --query accessToken)
+echo "##vso[task.setvariable variable=TOKEN;isOutput=true;issecret=true]$TOKEN"
+echo "Token generated: $TOKEN"
