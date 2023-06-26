19b4ac64ded3ae3c60b9bc674357fa05c0d3aee9
Rees Pozzi <reespozzi@icloud.com>
2023-06-26 15:16:06 +0100

    Revert "Tidy up pipeline script"
    This reverts commit e526b4d6551f34d11a62bad1a5bb40fad24275b3.

--- components/app-proxy/get-access-token.sh
+++ components/app-proxy/get-access-token.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
 
-az login --username $(azure-app-proxy-user-email) --password $(azure-app-proxy-user-password) --allow-no-subscriptions
-TOKEN=$(az account get-access-token --resource-type 'aad-graph' --scope 'https://proxy.cloudwebappproxy.net/registerapp/user_impersonation' --query accessToken)
-echo "##vso[task.setvariable variable=TOKEN;isOutput=true;issecret=true]$TOKEN"
-echo "Token generated: $TOKEN"
+az account get-access-token --resource-type 'aad-graph' --scope 'https://proxy.cloudwebappproxy.net/registerapp/user_impersonation'
