443c5fbc2527bcac2d479598d8008589b1ceba42
adusumillipraveen <adusumillipraveenkumar@gmail.com>
2023-07-06 22:04:37 +0100

    test dns

--- components/apps/main.tf
+++
@@ -1,9 +0,0 @@
-// TODO read from yaml config
-// TODO do we want to sync these to private DNS or use split brain DNS?
-resource "azurerm_dns_cname_record" "this" {
-  name                = "my-test-app.app-proxy-poc"
-  resource_group_name = "reformmgmtrg"
-  record              = "my-test-app-1dwnh4.msappproxy.net"
-  ttl                 = 3600
-  zone_name           = "sandbox.platform.hmcts.net"
-}
---
+++ components/dns/main.tf
@@ -0,0 +1,21 @@
+data "local_file" "configuration" {
+  filename = "${path.cwd}/../../apps.yaml"
+}
+
+locals {
+  apps = yamldecode(data.local_file.configuration.content).apps
+  cname_records = flatten([
+      for app in local.apps : split(".", replace("${app.externalUrl}", "https://", ""))[0]
+  ])
+}
+
+
+resource "azurerm_dns_cname_record" "this" {
+  for_each = toset(local.cname_records)
+
+  name                = each.value
+  zone_name           = "hmcts.net"
+  resource_group_name = "reformmgmtrg"
+  ttl                 = 3600
+  record              = "${each.value}-cjscommonplatform.msappproxy.net"
+}
