e96aa2f43e8459921011286534500627d873cfff
adusumillipraveen <adusumillipraveenkumar@gmail.com>
2023-07-07 11:26:30 +0100

    get zone name from url

--- components/dns/main.tf
+++ components/dns/main.tf
@@ -5,17 +5,20 @@ data "local_file" "configuration" {
 locals {
   apps = yamldecode(data.local_file.configuration.content).apps
   cname_records = flatten([
-    for app in local.apps : split(".", replace("${app.externalUrl}", "https://", ""))[0]
+    for app in local.apps : {
+      name      = split(".", replace("${app.externalUrl}", "https://", ""))[0],
+      zone_name = join(".", slice(split(".", "${app.externalUrl}"), 1, length(split(".", "${app.externalUrl}"))))
+    }
   ])
 }
 
 
 resource "azurerm_dns_cname_record" "this" {
-  for_each = toset(local.cname_records)
+  for_each = { for record in local.cname_records : record.name => record }
 
-  name                = each.value
-  zone_name           = "hmcts.net"
+  name                = each.value.name
+  zone_name           = each.value.zone_name
   resource_group_name = "reformmgmtrg"
   ttl                 = 3600
-  record              = "${each.value}-cjscommonplatform.msappproxy.net"
+  record              = "${each.value.name}-cjscommonplatform.msappproxy.net"
 }
