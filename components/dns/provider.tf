a36953377c38938b3b36b0130c04abd4cae3fb27
adusumillipraveen <adusumillipraveenkumar@gmail.com>
2023-07-07 10:29:35 +0100

    Add access policy for keyvault

--- components/dns/provider.tf
+++ components/dns/provider.tf
@@ -3,6 +3,14 @@ provider "azurerm" {
   subscription_id = "ed302caf-ec27-4c64-a05e-85731c3ce90e"
 }
 
+# Provider for Dynatrace
+provider "azurerm" {
+  alias = "cftptl"
+  features {}
+  skip_provider_registration = true
+  subscription_id            = "1baf5470-1c3e-40d3-a6f7-74bfbce4b348"
+}
+
 terraform {
   #  backend "azurerm" {}
   required_version = "1.5.2"
