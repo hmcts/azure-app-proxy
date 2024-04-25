d419bd6dde131a280b73234f45cb456859e9bf8f
Ciarán Carragher <ciaran.carragher2@justice.gov.uk>
2024-04-25 17:56:41 +0100

    Remove count and replace with for each

--- components/app-proxy/vm.tf
+++ components/app-proxy/vm.tf
@@ -28,6 +28,10 @@ resource "azurerm_subnet" "app_proxy" {
   virtual_network_name = var.app_proxy_vnet_name
 }
 
+locals {
+  app_proxy_vm_instances = toset(["app-proxy-0", "app-proxy-1", "app-proxy-2"])
+}
+
 # TODO auto-register VMs in DNS with private dns autoregistration
 module "virtual_machine" {
   providers = {
@@ -35,11 +39,11 @@ module "virtual_machine" {
     azurerm.cnp = azurerm.cnp
     azurerm.soc = azurerm.soc
   }
-  source  = "git::https://github.com/hmcts/terraform-module-virtual-machine.git?ref=master"
-  count   = var.vm_count
+  source  = "git::https://github.com/hmcts/terraform-module-virtual-machine.git?ref=DTSPO-17050-Upgrading-splunk-usf-version"
+  for_each = local.app_proxy_vm_instances
   vm_type = var.os_type
   # 15 Char name limit
-  vm_name           = "${var.product}-${count.index}"
+  vm_name           = each.key
   vm_resource_group = azurerm_resource_group.this.name
   env               = var.cnp_vault_env
   vm_admin_password = azurerm_key_vault_secret.vm_admin_password.value
@@ -50,7 +54,7 @@ module "virtual_machine" {
   vm_size           = "Standard_D2ds_v5"
   vm_version        = "latest"
   # 3 is max availability zones - round robin between them
-  vm_availabilty_zones = count.index % 3 + 1
+  vm_availabilty_zones = tonumber(element(flatten(regexall("([0-9]+)$", each.key)), 0)) + 1
 
   # Splunk
   install_splunk_uf   = true
@@ -72,14 +76,16 @@ module "virtual_machine" {
   nessus_groups  = var.nessus_groups
 
   # Custom app-proxy script
-  custom_script_extension_name = "app-proxy-onboarding-0${count.index}"
+  # custom_script_extension_name = "${each.value}-onboarding"
+  custom_script_extension_name = "app-proxy-onboarding-0${element(flatten(regexall("([0-9]+)$", each.key)), 0)}"
+  
   # update to point to branch
   additional_script_uri  = var.additional_script_uri
   additional_script_name = "${var.additional_script_name} -TenantId ${data.azurerm_client_config.this.tenant_id} -Username ${data.azurerm_key_vault_secret.vm_user_email.value} -Password ${data.azurerm_key_vault_secret.vm_user_password.value}"
 
   privateip_allocation           = "Dynamic"
   accelerated_networking_enabled = true
-  tags                           = count.index == 2 ? module.shutdowntags.common_tags : module.tags.common_tags
+  tags                           = each.value == 2 ? module.shutdowntags.common_tags : module.tags.common_tags
 }
 
 data "azurerm_client_config" "this" {}
