da931461483a4d251865466592b863d759b9cc03
ciaranca <c.carragher@kainos.com>
2024-05-07 21:15:54 +0100

    Remove count and replace with for each (#57)
    * Remove count and replace with for each
    
    * tf fmt

---
+++ components/app-proxy/moved.tf
@@ -0,0 +1,89 @@
+moved {
+  from = module.virtual_machine[0].azurerm_network_interface.vm_nic
+  to   = module.virtual_machine["app-proxy-0"].azurerm_network_interface.vm_nic
+}
+
+moved {
+  from = module.virtual_machine[0].azurerm_windows_virtual_machine.winvm[0]
+  to   = module.virtual_machine["app-proxy-0"].azurerm_windows_virtual_machine.winvm[0]
+}
+
+moved {
+  from = module.virtual_machine[1].azurerm_network_interface.vm_nic
+  to   = module.virtual_machine["app-proxy-1"].azurerm_network_interface.vm_nic
+}
+
+moved {
+  from = module.virtual_machine[1].azurerm_windows_virtual_machine.winvm[0]
+  to   = module.virtual_machine["app-proxy-1"].azurerm_windows_virtual_machine.winvm[0]
+}
+
+moved {
+  from = module.virtual_machine[2].azurerm_network_interface.vm_nic
+  to   = module.virtual_machine["app-proxy-2"].azurerm_network_interface.vm_nic
+}
+
+moved {
+  from = module.virtual_machine[2].azurerm_windows_virtual_machine.winvm[0]
+  to   = module.virtual_machine["app-proxy-2"].azurerm_windows_virtual_machine.winvm[0]
+}
+
+moved {
+  from = module.virtual_machine[0].module.vm-bootstrap[0].azurerm_virtual_machine_extension.azure_monitor[0]
+  to   = module.virtual_machine["app-proxy-0"].module.vm-bootstrap[0].azurerm_virtual_machine_extension.azure_monitor[0]
+}
+
+moved {
+  from = module.virtual_machine[0].module.vm-bootstrap[0].azurerm_virtual_machine_extension.custom_script[0]
+  to   = module.virtual_machine["app-proxy-0"].module.vm-bootstrap[0].azurerm_virtual_machine_extension.custom_script[0]
+}
+
+moved {
+  from = module.virtual_machine[0].module.vm-bootstrap[0].azurerm_virtual_machine_extension.dynatrace_oneagent[0]
+  to   = module.virtual_machine["app-proxy-0"].module.vm-bootstrap[0].azurerm_virtual_machine_extension.dynatrace_oneagent[0]
+}
+
+moved {
+  from = module.virtual_machine[0].module.vm-bootstrap[0].azurerm_virtual_machine_extension.endpoint_protection[0]
+  to   = module.virtual_machine["app-proxy-0"].module.vm-bootstrap[0].azurerm_virtual_machine_extension.endpoint_protection[0]
+}
+
+moved {
+  from = module.virtual_machine[1].module.vm-bootstrap[0].azurerm_virtual_machine_extension.azure_monitor[0]
+  to   = module.virtual_machine["app-proxy-1"].module.vm-bootstrap[0].azurerm_virtual_machine_extension.azure_monitor[0]
+}
+
+moved {
+  from = module.virtual_machine[1].module.vm-bootstrap[0].azurerm_virtual_machine_extension.custom_script[0]
+  to   = module.virtual_machine["app-proxy-1"].module.vm-bootstrap[0].azurerm_virtual_machine_extension.custom_script[0]
+}
+
+moved {
+  from = module.virtual_machine[1].module.vm-bootstrap[0].azurerm_virtual_machine_extension.dynatrace_oneagent[0]
+  to   = module.virtual_machine["app-proxy-1"].module.vm-bootstrap[0].azurerm_virtual_machine_extension.dynatrace_oneagent[0]
+}
+
+moved {
+  from = module.virtual_machine[1].module.vm-bootstrap[0].azurerm_virtual_machine_extension.endpoint_protection[0]
+  to   = module.virtual_machine["app-proxy-1"].module.vm-bootstrap[0].azurerm_virtual_machine_extension.endpoint_protection[0]
+}
+
+moved {
+  from = module.virtual_machine[2].module.vm-bootstrap[0].azurerm_virtual_machine_extension.azure_monitor[0]
+  to   = module.virtual_machine["app-proxy-2"].module.vm-bootstrap[0].azurerm_virtual_machine_extension.azure_monitor[0]
+}
+
+moved {
+  from = module.virtual_machine[2].module.vm-bootstrap[0].azurerm_virtual_machine_extension.custom_script[0]
+  to   = module.virtual_machine["app-proxy-2"].module.vm-bootstrap[0].azurerm_virtual_machine_extension.custom_script[0]
+}
+
+moved {
+  from = module.virtual_machine[2].module.vm-bootstrap[0].azurerm_virtual_machine_extension.dynatrace_oneagent[0]
+  to   = module.virtual_machine["app-proxy-2"].module.vm-bootstrap[0].azurerm_virtual_machine_extension.dynatrace_oneagent[0]
+}
+
+moved {
+  from = module.virtual_machine[2].module.vm-bootstrap[0].azurerm_virtual_machine_extension.endpoint_protection[0]
+  to   = module.virtual_machine["app-proxy-2"].module.vm-bootstrap[0].azurerm_virtual_machine_extension.endpoint_protection[0]
+}
