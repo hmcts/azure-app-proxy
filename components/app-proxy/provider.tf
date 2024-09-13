d6f6ed9bee7ad95c9f58100703f62f38224f44bc
Ieuan Byers <ieuan.byers@justice.gov.uk>
2024-09-13 08:20:28 +0100

    Added new provider config for dcr

--- components/app-proxy/provider.tf
+++ components/app-proxy/provider.tf
@@ -20,6 +20,12 @@ provider "azurerm" {
   subscription_id            = var.cnp_vault_sub
 }
 
+provider "azurerm" {
+  alias = "dcr"
+  features {}
+  subscription_id = var.env == "prod" || var.env == "production" ? "8999dec3-0104-4a27-94ee-6588559729d1" : var.env == "sbox" || var.env == "sandbox" ? "bf308a5c-0624-4334-8ff8-8dca9fd43783" : "1c4f0704-a29e-403d-b719-b90c34ef14c9"
+}
+
 terraform {
   backend "azurerm" {}
   required_version = "1.4.0"
