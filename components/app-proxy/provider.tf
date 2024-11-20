3dbd054eda7fb665c532526dd52bba810fa43aea
Narayn Manoharan <narayn.manoharan@justice.gov.uk>
2024-11-20 15:39:30 +0000

    Updating Provider Version for Compatiblity with New Updates to Module

--- components/app-proxy/provider.tf
+++ components/app-proxy/provider.tf
@@ -29,7 +29,7 @@ provider "azurerm" {
 
 terraform {
   backend "azurerm" {}
-  required_version = "1.4.0"
+  required_version = "1.9.0"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
